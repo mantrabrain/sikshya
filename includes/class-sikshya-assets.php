55e63727f149b620c6410b4496ddd811d42ca7d8
mantrabrain <mantrabrain@gmail.com>
2020-04-30 12:16:15 +0545

    update

--- includes/class-sikshya-assets.php
+++ includes/class-sikshya-assets.php
@@ -13,8 +13,6 @@ class Sikshya_Assets
 
         wp_enqueue_style('dashicons');
 
-        wp_enqueue_style('sikshya-grid-style', SIKSHYA_ASSETS_URL . '/css/sikshya-grid.css', array(), SIKSHYA_VERSION);
-
         wp_enqueue_style(SIKSHYA_COURSES_CUSTOM_POST_TYPE . '-sikshya', SIKSHYA_ASSETS_URL . '/css/sikshya.css', false, SIKSHYA_VERSION);
         wp_enqueue_script('jquery-ui-core');
         //wp_enqueue_script('jquery-plugin', SIKSHYA_ASSETS_URL . '/public/js/vendor/jquery.plugin.min.js', array(), SIKSHYA_VERSION);
@@ -28,7 +26,7 @@ class Sikshya_Assets
         // Enqueue styles
         wp_enqueue_style('sikshya-font-awesome-style', SIKSHYA_ASSETS_URL . '/vendor/font-awesome/css/font-awesome.css', array(), SIKSHYA_VERSION);
         wp_enqueue_style('sikshya-ionicons-style', SIKSHYA_ASSETS_URL . '/vendor/ionicons//css/ionicons.min.css', array(), SIKSHYA_VERSION);
-        
+
     }
 }
 
