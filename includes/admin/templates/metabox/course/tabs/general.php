8905ae5fdec9d91bdae1ff312b15eeb65006470b
mantrabrain <mantrabrain@gmail.com>
2020-05-09 16:27:53 +0545

    update

--- includes/admin/templates/metabox/course/tabs/general.php
+++ includes/admin/templates/metabox/course/tabs/general.php
@@ -1,1 +1,11 @@
-<h1>General</h1>
+<div class="sikshya-field-wrap">
+    <div class="sikshya-field-label">
+        <label for="sikshya_course_duration">Group
+            Size </label>
+    </div>
+    <div class="sikshya-field-content">
+        <input class="widefat" id="sikshya_course_duration" name="sikshya_course_duration" type="number" value=""
+               placeholder="Duration">
+    </div>
+
+</div>
