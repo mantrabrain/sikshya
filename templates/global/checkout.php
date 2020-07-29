6dd086bafba04a867265746c7fd0107743fc5e08
ughimire <me.umeshuser@gmail.com>
2020-07-29 16:00:12 +0545

    udpate

--- templates/global/checkout.php
+++ templates/global/checkout.php
@@ -1,7 +1,6 @@
-<form name="checkout" method="post" class="checkout woocommerce-checkout"
-	  action="https://demo.themeum.com/plugins/tutor/checkout/" enctype="multipart/form-data" novalidate="novalidate">
-
-
+<form name="sikshya-checkout" method="post" class="sikshya-checkout woocommerce-sikshya-checkout"
+	  action="https://demo.themeum.com/plugins/tutor/sikshya-checkout/"
+	  enctype="multipart/form-data" novalidate="novalidate">
 	<?php
 	sikshya_load_template('global.checkout-form', array('sikshya_checkout_items' => array()));
 
