1cd877771d046321de60798d64e70b7e17e1f61e
ughimire <me.umeshuser@gmail.com>
2020-10-02 22:43:10 +0545

    importer fix

--- includes/classes/class-sikshya-core-importer.php
+++ includes/classes/class-sikshya-core-importer.php
@@ -72,6 +72,10 @@ class Sikshya_Core_Importer
 
 				$updated_post_ids_mapping[$sikshya_custom_posts['ID']] = $sik_post_id;
 
+
+				$image_attributes = isset($sikshya_custom_posts['image_attributes']) ? $sikshya_custom_posts['image_attributes'] : array();
+
+				$this->import_image($sik_post_id, $image_attributes);
 			}
 
 
@@ -107,6 +111,43 @@ class Sikshya_Core_Importer
 		} catch (Exception $e) {
 			return false;
 		}
+
+	}
+
+	private function import_image($post_id, $image_attr)
+	{
+		$url = isset($image_attr[0]) ? $image_attr[0] : null;
+		if (is_null($url)) {
+			return;
+		}
+		$pathinfo = pathinfo($url);
+		$filename = $pathinfo['filename'] . '.' . $pathinfo['extension'];
+
+		$uploaddir = wp_upload_dir();
+		$uploadfile = $uploaddir['path'] . '/' . $filename;
+
+		$contents = file_get_contents($url);
+		$savefile = fopen($uploadfile, 'w');
+		fwrite($savefile, $contents);
+		fclose($savefile);
+
+
+		$wp_filetype = wp_check_filetype(basename($filename), null);
+
+		$attachment = array(
+			'post_mime_type' => $wp_filetype['type'],
+			'post_title' => $filename,
+			'post_content' => '',
+			'post_status' => 'inherit'
+		);
+
+		$attach_id = wp_insert_attachment($attachment, $uploadfile);
+		$imagenew = get_post($attach_id);
+		$fullsizepath = get_attached_file($imagenew->ID);
+		$attach_data = wp_generate_attachment_metadata($attach_id, $fullsizepath);
+		wp_update_attachment_metadata($attach_id, $attach_data);
+		update_post_meta($post_id, '_thumbnail_id', $attach_id);
+
 
 	}
 }
