060a10b005fbcbe63c972ce350cb807eee5f795d
Umesh Ghimire <me.umeshuser@gmail.com>
2021-10-10 16:47:28 +0545

    unused comment removed

--- includes/class-sikshya-install.php
+++ includes/class-sikshya-install.php
@@ -362,68 +362,6 @@ class Sikshya_Install
 			$wp_roles = new WP_Roles(); // @codingStandardsIgnoreLine
 		}
 
-//        // Dummy gettext calls to get strings in the catalog.
-//        /* translators: user role */
-//        _x('Customer', 'User role', 'sikshya');
-//        /* translators: user role */
-//        _x('Shop manager', 'User role', 'sikshya');
-//
-//        // Customer role.
-//        add_role(
-//            'customer',
-//            'Customer',
-//            array(
-//                'read' => true,
-//            )
-//        );
-
-		// Shop manager role.
-		/*add_role(
-			'shop_manager',
-			'Shop manager',
-			array(
-				'level_9' => true,
-				'level_8' => true,
-				'level_7' => true,
-				'level_6' => true,
-				'level_5' => true,
-				'level_4' => true,
-				'level_3' => true,
-				'level_2' => true,
-				'level_1' => true,
-				'level_0' => true,
-				'read' => true,
-				'read_private_pages' => true,
-				'read_private_posts' => true,
-				'edit_posts' => true,
-				'edit_pages' => true,
-				'edit_published_posts' => true,
-				'edit_published_pages' => true,
-				'edit_private_pages' => true,
-				'edit_private_posts' => true,
-				'edit_others_posts' => true,
-				'edit_others_pages' => true,
-				'publish_posts' => true,
-				'publish_pages' => true,
-				'delete_posts' => true,
-				'delete_pages' => true,
-				'delete_private_pages' => true,
-				'delete_private_posts' => true,
-				'delete_published_pages' => true,
-				'delete_published_posts' => true,
-				'delete_others_posts' => true,
-				'delete_others_pages' => true,
-				'manage_categories' => true,
-				'manage_links' => true,
-				'moderate_comments' => true,
-				'upload_files' => true,
-				'export' => true,
-				'import' => true,
-				'list_users' => true,
-				'edit_theme_options' => true,
-			)
-		);*/
-
 		$capabilities = self::get_core_capabilities();
 
 		foreach ($capabilities as $cap_group) {
